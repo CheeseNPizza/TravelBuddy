6e29888820591fceffef9801651dff4e2e801448
CheeseNPizza <90838394+CheeseNPizza@users.noreply.github.com>
2023-09-03 23:46:44 +0800

    Update PlaceAutocompleteAdapter.java
    add documentation

--- app/src/main/java/my/edu/utar/groupassignment/adapter/PlaceAutocompleteAdapter.java
+++ app/src/main/java/my/edu/utar/groupassignment/adapter/PlaceAutocompleteAdapter.java
@@ -1,3 +1,10 @@
+/**
+ * PlaceAutocompleteAdapter is an ArrayAdapter used for auto-suggesting place names
+ * based on user input. It utilizes Google Places AutoComplete API to fetch place suggestions.
+ *
+ * @author Lee Teck Junn
+ * @version 1.0
+ */
 package my.edu.utar.groupassignment.adapter;
 
 import android.content.Context;
@@ -12,29 +19,36 @@ import java.util.ArrayList;
 
 public class PlaceAutocompleteAdapter extends ArrayAdapter implements Filterable {
 
+    // ArrayList to store the auto-suggested place names
     ArrayList<String> results;
 
+    // Resource ID for the adapter
     int resource;
     Context context;
 
+    // Instance of the PlaceApi class for fetching place suggestions
     PlaceApi placeApi = new PlaceApi();
 
+    // Constructor for PlaceAutocompleteAdapter.
     public PlaceAutocompleteAdapter(Context context, int resId){
         super(context,resId);
         this.context = context;
         this.resource = resId;
     }
 
+    // Get the count of auto-suggested place names.
     @Override
     public int getCount(){
         return results.size();
     }
 
+    // Get a specific auto-suggested place name at the given position.
     @Override
     public String getItem(int pos){
         return results.get(pos);
     }
 
+    // Get a Filter object that handles auto-suggestions based on user input.
     public Filter getFilter(){
         Filter filter = new Filter() {
             @Override
@@ -42,8 +56,10 @@ public class PlaceAutocompleteAdapter extends ArrayAdapter implements Filterable
                 FilterResults filterResults = new FilterResults();
                 if (charSequence!= null){
                     try {
+                        // Use the PlaceApi to fetch auto-suggestions based on user input
                         results = placeApi.autoComplete(charSequence.toString());
                     } catch (MalformedURLException e) {
+                        // Handle any URL-related exceptions by throwing a runtime exception
                         throw new RuntimeException(e);
                     }
 
@@ -56,9 +72,11 @@ public class PlaceAutocompleteAdapter extends ArrayAdapter implements Filterable
             @Override
             protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
                 if (filterResults != null && filterResults.count > 0 ){
+                    // Notify the adapter that data has changed, and auto-suggestions should be displayed
                     notifyDataSetChanged();
                 }
                 else {
+                    // Notify the adapter that the data set is invalid, and no suggestions should be displayed
                     notifyDataSetInvalidated();
                 }
             }
