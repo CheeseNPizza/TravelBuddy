2cdf93d6d13e6a017954456d71865b98fc495704
CheeseNPizza <90838394+CheeseNPizza@users.noreply.github.com>
2023-09-03 23:40:41 +0800

    Update DetailActivity.java
    add documentation

--- app/src/main/java/my/edu/utar/groupassignment/DetailActivity.java
+++ app/src/main/java/my/edu/utar/groupassignment/DetailActivity.java
@@ -1,3 +1,7 @@
+/**
+ * @author Lee Teck Junn
+ * @version 1.0
+ */
 package my.edu.utar.groupassignment;
 
 import androidx.appcompat.app.AlertDialog;
@@ -18,6 +22,7 @@ import my.edu.utar.groupassignment.structure.Place;
 
 public class DetailActivity extends AppCompatActivity {
 
+    // Declaring variables
     private TextView text_name, text_address, text_rating, text_price_level, text_description;
     private Place place;
     private ImageView photo, return_btn;
@@ -30,20 +35,24 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        // Initialize UI elements
         initView();
+        // Set data for UI elements based on intent data
         setVariable();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
+        // Set a click listener for the "book_btn" button
         Button book_btn = findViewById(R.id.book_btn);
         book_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                show_dialog();
+                show_dialog(); // Show a dialog when the button is clicked
             }
         });
     }
 
+    // Set data for UI elements based on intent data
     private void setVariable() {
         place = (Place) getIntent().getSerializableExtra("object");
 
@@ -53,15 +62,18 @@ public class DetailActivity extends AppCompatActivity {
         text_price_level.setText(place.getPrice_level());
         text_description.setText(place.getDescription());
 
+        // Load an image using Glide library
         Glide.with(this)
                 .load(place.getPhoto())
                 .into(photo);
 
         web_url = place.getWeb_url();
 
+        // Set a click listener for the "return_btn" button to finish the activity
         return_btn.setOnClickListener(v -> finish());
     }
 
+    // Initialize UI elements
     private void initView() {
         text_name = findViewById(R.id.detail_name);
         text_address = findViewById(R.id.detail_address);
@@ -72,27 +84,33 @@ public class DetailActivity extends AppCompatActivity {
         return_btn = findViewById(R.id.return_btn);
     }
 
+    // Show a dialog with a WebView to display a web page
     private void show_dialog(){
 
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
+        // Load a web page URL in the WebView
         WebView webView = new WebView(this);
         webView.loadUrl(web_url);
         webView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url){
-                view.loadUrl(url);
+                // Open links in the same WebView
+                view.loadUrl(url); 
                 return true;
             }
         });
 
+        // Set the WebView as the content of the dialog
         alert.setView(webView);
         alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                // Dismiss the dialog when the "Close" button is clicked
                 dialog.dismiss();
             }
         });
+        // Show the dialog
         alert.show();
     }
 }
